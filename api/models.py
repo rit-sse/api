5daa3e2f776251131ab80a52192faa7b7aaec0cf
Galen Guyer <galen@galenguyer.com>
2021-08-05 16:02:31 -0700

    add officers model and db migration

--- api/models.py
+++ api/models.py
@@ -10,3 +10,18 @@ class GoLink(db.Model):
 
     def __repr__(self):
         return "<GoLink {}>".format(self.short_link)
+
+
+class Officer(db.Model):
+    __tablename__ = "officers"
+    id = db.Column(db.Integer, primary_key=True)
+    committee = db.Column(db.String(64), nullable=False)
+    rit_email = db.Column(db.String(32), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    bio = db.Column(db.Text)
+    is_primary = db.Column(db.Boolean)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return "<Officer {}>".format(self.id)
