09b1f46ad70e4342ea26e6c64021b3c3d3e787d3
Galen Guyer <galen@galenguyer.com>
2021-08-18 14:15:42 -0400

    rename rit_email to rit_dce

--- api/models.py
+++ api/models.py
@@ -16,8 +16,8 @@ class Officer(db.Model):
     __tablename__ = "officers"
     id = db.Column(db.Integer, primary_key=True)
     committee = db.Column(db.String(64), nullable=False)
-    rit_email = db.Column(db.String(32), nullable=False)
-    name = db.Column(db.String(64), nullable=False)
+    rit_dce = db.Column(db.String(32), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
     bio = db.Column(db.Text)
     is_primary = db.Column(db.Boolean)
     start_date = db.Column(db.DateTime, nullable=False)
@@ -31,7 +31,7 @@ class Membership(db.Model):
     __tablename__ = "memberships"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
-    rit_email = db.Column(db.String(32), nullable=False)
+    rit_dce = db.Column(db.String(32), nullable=False)
     reason = db.Column(db.Text)
     given = db.Column(db.DateTime, nullable=False)
     expires = db.Column(db.DateTime, nullable=False)
