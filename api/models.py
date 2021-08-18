393f2650f4830039fcdf2f714186bcc41ce9402f
Galen Guyer <galen@galenguyer.com>
2021-08-18 14:55:00 -0400

    inline static typing

--- api/models.py
+++ api/models.py
@@ -17,24 +17,15 @@ class GoLink(db.Model):
 # dataclass usage: https://stackoverflow.com/a/57732785
 @dataclass
 class Officer(db.Model):
-    id: int
-    committee: str
-    rit_dce: str
-    name: str
-    bio: str
-    is_primary: bool
-    start_date: datetime
-    end_date: datetime
-
     __tablename__ = "officers"
-    id = db.Column(db.Integer, primary_key=True)
-    committee = db.Column(db.String(64), nullable=False)
-    rit_dce = db.Column(db.String(32), nullable=False)
-    name = db.Column(db.String(128), nullable=False)
-    bio = db.Column(db.Text)
-    is_primary = db.Column(db.Boolean)
-    start_date = db.Column(db.DateTime, nullable=False)
-    end_date = db.Column(db.DateTime, nullable=False)
+    id: int = db.Column(db.Integer, primary_key=True)
+    committee: str = db.Column(db.String(64), nullable=False)
+    rit_dce: str = db.Column(db.String(32), nullable=False)
+    name: str = db.Column(db.String(128), nullable=False)
+    bio: str = db.Column(db.Text)
+    is_primary: bool = db.Column(db.Boolean)
+    start_date: datetime = db.Column(db.DateTime, nullable=False)
+    end_date: datetime = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         return "<Officer {}>".format(self.id)
