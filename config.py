995f5d03a849a8c142d71c4d857aba366c771816
Galen Guyer <galen@galenguyer.com>
2021-08-26 17:34:39 -0400

    rename database url to uri

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv(dotenv_path)
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "".join(secrets.token_hex(16))
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+        "DATABASE_URI"
     ) or "sqlite:///" + os.path.join(dirname(__file__), "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
